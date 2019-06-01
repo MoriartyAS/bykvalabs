58c7509fef9af2c9c2c5cdc759cf5048121e8f13
Mor <lyckovaanna@mail.ru>
2019-06-01 03:17:33 +0300

    Create suz15.sh

---
+++ Лаба1/suz14.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo ($(ls part1/)$(ls ../john/Documents/part2/)$(ls ../j
+ohn/Desktop/part3/)
---
+++ Лаба1/suz19.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p Documents/projects/lab19/using/simple/bash/commands
+/home/suzen 
+cd Documents/projects/lab19/using/simple/bash/commands
+ls
